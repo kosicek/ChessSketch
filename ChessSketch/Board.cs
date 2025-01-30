c73a37cd87c58775f11b67ad0b57769f7cc76bc0
Kosicelk <kosic5@centrum.cz>
2025-01-30 01:57:53 +0100

    a

---
+++ ChessSketch/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessSketch
+{
+    internal class Board
+    {
+        static public ChessPiece[,] ChessBoard = new ChessPiece[,]
+        { { new ChessPiece("Black","Rook"), new ChessPiece("Black","Knight"), new ChessPiece("Black","Bishop"), new ChessPiece("Black","Queen"), new ChessPiece("Black","King"), new ChessPiece("Black","Bishop"), new ChessPiece("Black","Knight"), new ChessPiece("Black","Rook")}
+        , { new ChessPiece("Black","Pawn"), new ChessPiece("Black","Pawn"), new ChessPiece("Black","Pawn"), new ChessPiece("Black","Pawn"), new ChessPiece("Black","Pawn"), new ChessPiece("Black","Pawn"), new ChessPiece("Black","Pawn"), new ChessPiece("Black","Pawn")}
+        , { new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"),}
+        , { new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"),}
+        , { new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"),}
+        , { new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"), new ChessPiece("Empty","Empty"),}
+        , { new ChessPiece("White","Pawn"), new ChessPiece("White","Pawn"), new ChessPiece("White","Pawn"), new ChessPiece("White","Pawn"), new ChessPiece("White","Pawn"), new ChessPiece("White","Pawn"), new ChessPiece("White","Pawn"), new ChessPiece("White","Pawn")}
+        , { new ChessPiece("White","Rook"), new ChessPiece("White","Knight"), new ChessPiece("White","Bishop"), new ChessPiece("White","Queen"), new ChessPiece("White","King"), new ChessPiece("White","Bishop"), new ChessPiece("White","Knight"), new ChessPiece("White","Rook")}
+};
+
+    }
+}
---
+++ ChessSketch/ChessPiece.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessSketch
+{
+    class ChessPiece
+    {
+        private bool _hasMoved = false;
+        private string _team = "None"; // Black Or White
+        private string _piece = "None"; // Official Names => King,Queen,Bishop,Rook,Knight,Pawn,Null
+
+        public ChessPiece(string team, string piece)
+        {
+            this._team = Team;
+            this._piece = Piece;
+            this._hasMoved = false;
+        }
+
+        public string Team
+        {
+            get { return _team; }
+            set { _team = value; }
+        }
+
+        public string Piece
+        {
+            get { return _piece; }
+            set { _piece = value; }
+        }
+
+        public bool HasMoved
+        {
+            get { return _hasMoved; }
+            set { _hasMoved = value; }
+        }
+
+        /*public ReturnArt()
+        {
+            if (_team != "Null" && _piece != "Null")
+            {
+                return new BitmapImage(new Uri("/Assets/" + _team + "_" + _piece + ".png", UriKind.Relative));
+            }
+            return null;
+        }*/
+    }
+}
